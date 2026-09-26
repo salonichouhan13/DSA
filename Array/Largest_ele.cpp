284dea0696556896009f53c8ce65c07bcf27e387
Saloni Chouhan <salonichouhan803@gmail.com>
2026-09-26 12:46:15 +0530

    revision

--- Array/Largest_ele.cpp
+++ Array/Largest_ele.cpp
@@ -1,28 +1,56 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int LargestElement(vector<int> array,int n){
+//   int largest = array[0];
+//   for(int i= 0; i<n; i++){
+//     if(array[i]>largest){
+//       largest = array[i];
+//     }
+//   }
+//   return largest;
+// }
+
+// int main(){
+//   int n;
+//   cout<<"enter size of array";
+//   cin>>n;
+//   vector<int> array(n);
+
+//   //taking input
+
+//   for(int i=0; i<n; i++){
+//     cin>>array[i];
+//   }
+//   //call function
+//   cout<<"largest element is = "<<LargestElement(array,n)<<endl;
+ 
+//   return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
-int LargestElement(vector<int> array,int n){
-  int largest = array[0];
-  for(int i= 0; i<n; i++){
-    if(array[i]>largest){
-      largest = array[i];
+int LargestElement(vector<int> Array,int n){
+  int Largest = Array[0];
+  for(int i = 0; i<n; i++){
+    if(Array[i]>Largest){
+      Largest=Array[i];
     }
+   
   }
-  return largest;
+   return Largest;
 }
 
 int main(){
   int n;
-  cout<<"enter size of array";
+  cout<<"Enter Size Of An Array"<<endl;
   cin>>n;
-  vector<int> array(n);
-
-  //taking input
-
+  vector<int> Array(n);
+  //takiing input
   for(int i=0; i<n; i++){
-    cin>>array[i];
-  }
-  //call function
-  cout<<"largest element is = "<<LargestElement(array,n)<<endl;
- 
+  cin>>Array[i];}
+  cout<<"Largest Element Is" <<LargestElement(Array,n)<<endl;
+
   return 0;
+
 }
---
+++ Recursion/num_palindrome.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class solution{
+  public:
+  bool isPalindrome(int n){
+  if(n<0)
+    return false;
+    int original = n ;
+    long long rev = 0;
+    while(n>0){
+      int digit = n%10;
+      rev = rev
+      
+    }
+  }
+};
